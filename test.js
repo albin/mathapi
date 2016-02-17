600624a2a81097abd7cfc1d88dd47ffcce44b30e
Derek <der.tse@gmail.com>
2016-02-17 17:21:54 +0100

    added pyth theorem test

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var should = chai.should();
 chai.use(chaiHttp);
 /*
 This comment will tell you how the test will be constructed.
-Add you test bellow, you need three test's (it), what they will check on your math object is up too you.
+Add your test below, you need three test's (it), what they will check on your math object is up to you.
 Follow the structure down below in the commented section, just an example how to test math-min gives a number.
 
 
@@ -32,10 +32,53 @@ it('should show if result equals number, when adding', function(done){
 
 //End
 */
+/*Add your describe here, dont forget that you need 3 it(). Here is a template:
 
-describe('Data API', function(){
-	/*Add your it here, dont forget that you need 3 seprate, keep em together and seperated by a comment with your name.*/	
+describe('Function name', function(){
+	it();
+	it();
+	it();
+});
 
+*/	
 
+describe("Pythagora's Theorem' by Derek", function(){
+	it('should return a SINGLE object with status OK on /pyth/<a>/<b>/<c> GET', function (done){
+		chai.request('http://127.0.0.1:3000').get('/pyth/3/b/5').end(function (err, res){
+			res.should.have.status(200);
+			res.should.be.json;
+			res.body.should.be.an('object');
+			res.body.should.have.property('status');
+			res.body.status.should.be.a('string');
+			res.body.status.should.equal('OK')
+			res.body.should.have.property('result');
+			res.body.result.should.be.a('number');
+			done();
+		})
+	});
+	it('should return a SINGLE object with status OK on /pyth/<a>/<b>/<c> GET', function (done){
+		chai.request('http://127.0.0.1:3000').get('/pyth/3.5/b/5.3').end(function (err, res){
+			res.should.have.status(200);
+			res.should.be.json;
+			res.body.should.be.an('object');
+			res.body.should.have.property('status');
+			res.body.status.should.be.a('string');
+			res.body.status.should.equal('OK')
+			res.body.should.have.property('result');
+			res.body.result.should.be.a('number');
+			done();
+		})
+	});
+	it('should return a SINGLE object with status ERR on /pyth/<a>/<b>/<c> GET', function (done){
+		chai.request('http://127.0.0.1:3000').get('/pyth/a/b/c').end.(function (err, res){
+			res.should.have.status(200);
+			res.should.be.json;
+			res.body.should.be.an('object');
+			res.body.should.have.property('status');
+			res.body.status.should.be.a('string');
+			res.body.status.should.equal('ERR')
+			done();
 
+		});
+	});
 });
