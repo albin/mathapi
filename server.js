33739f79390c9951d455e9271b607c2fe9edd233
Derek <der.tse@gmail.com>
2016-02-17 15:31:01 +0100

    added pyth theorem in server.js

--- server.js
+++ server.js
@@ -23,6 +23,30 @@ Float numbers seperated by ".", example /pow/1.5/2.5
 app.use(express.static("public"));
 
 app.use('/', router);
+
+app.get('/pyth/:a/:b/:C', function (req, res){
+	var a = parseFloat(req.params.a);
+	var b = parseFloat(req.params.b);
+	var c = parseFloat(req.params.c);
+	var result;
+	if (isNaN(a) && !isNaN(b) && !isNaN(c)) {
+		result = Math.sqrt((c*c)-(b*b));
+		res.json({status: 'OK', result: result});
+	}
+	else if (!isNaN(a) && isNaN(b) && !isNaN(c)) {
+		result = Math.sqrt((c*c)-(a*a));
+		res.json({status: 'OK', result: result});
+	}
+	else if (!isNaN(a) && !isNaN(b) && isNaN(c)) {
+		result = Math.sqrt((a*a)+(b*b));
+		res.json({status: 'OK', result: result});
+	}
+	else{
+		res.json(status: 'ERR')
+	}
+
+
+});
 app.listen(port);
 console.log('Up and runing at port' + port);
 
