6d2d847ae45579eacc357886e1fbfe5ab9087df1
Olle Bengtsson <ollebsson2@gmail.com>
2016-02-18 14:31:14 +0100

    removed accidental copy of pyth route

--- server.js
+++ server.js
@@ -156,29 +156,6 @@ app.get('/ceil/:x', function (req, res) {
 //Dont add anything below this line.
 app.use(express.static("public"));
 app.use('/', router);
-
-app.get('/pyth/:a/:b/:c', function (req, res){
-	var a = parseFloat(req.params.a);
-	var b = parseFloat(req.params.b);
-	var c = parseFloat(req.params.c);
-	var result;
-	if (isNaN(a) && !isNaN(b) && !isNaN(c)) {
-		result = Math.sqrt((c*c)-(b*b));
-		res.json({status: 'OK', result: result});
-	}
-	else if (!isNaN(a) && isNaN(b) && !isNaN(c)) {
-		result = Math.sqrt((c*c)-(a*a));
-		res.json({status: 'OK', result: result});
-	}
-	else if (!isNaN(a) && !isNaN(b) && isNaN(c)) {
-		result = Math.sqrt((a*a)+(b*b));
-		res.json({status: 'OK', result: result});
-	}
-	else{
-		res.json({status: 'ERR'})
-	}
-});
-
 app.listen(port);
 console.log('Up and runing at port' + port);
 
